d536c7b374aaa60d030c6d2804c9f13e6eda5a30
Jheimys Bassani <pd4.rbcx@gmail.com>
2025-04-28 09:45:03 +0000

    Display books on screen with filter method

--- app/forEach.js
+++ app/forEach.js
@@ -1,6 +1,8 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivrosNatela(listaDeLivros){
+    elementoParaInserirLivros.innerHTML = ''
+
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
