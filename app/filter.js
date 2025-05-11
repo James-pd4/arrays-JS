b77e77f1a37078fe0e38de04c60c57d475792e35
Jheimys Bassani <pd4.rbcx@gmail.com>
2025-05-11 17:15:03 +0000

    resolving conflict

--- app/filter.js
+++ app/filter.js
@@ -1,19 +1,15 @@
 const botoes = document.querySelectorAll('.btn')
-<<<<<<< HEAD
-=======
 
->>>>>>> metodo_filter
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-<<<<<<< HEAD
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
-    exibirOsLivrosNaTela(livrosFiltrados)
-=======
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quandade > 0) : livros.filter(livro => livro.categoria == categoria)
+
+    let livrosFiltrados = categoria == 'disponivel'
+        ? livros.filter(livro => livro.quandade > 0)
+        : livros.filter(livro => livro.categoria == categoria)
 
     exibirOsLivrosNatela(livrosFiltrados)
->>>>>>> metodo_filter
+
 }
