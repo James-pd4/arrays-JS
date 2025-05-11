d4e87b548b12978ced66c96e4804e9283cc4d05a
Jheimys Bassani <pd4.rbcx@gmail.com>
2025-05-11 16:49:40 +0000

    adding unavailable books filter

--- app/filter.js
+++ app/filter.js
@@ -2,10 +2,10 @@ const botoes = document.querySelectorAll('.btn')
 
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
-function  filtrarLivros(){
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria === categoria )
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quandade > 0) : livros.filter(livro => livro.categoria == categoria)
 
-    exibirOsLivrosNatela(livrosFiltrados )
+    exibirOsLivrosNatela(livrosFiltrados)
 }
